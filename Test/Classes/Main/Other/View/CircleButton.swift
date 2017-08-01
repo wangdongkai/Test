fa9d3274e1547e9f47eea085eb9cd50bd6ec4239
wangdongkai <973153384@qq.com>
2017-08-01 17:57:37 +0800

    1.新建圆形按钮类。

---
+++ Test/Classes/Main/Other/View/CircleButton.swift
@@ -0,0 +1,23 @@
+//
+//  CircleButton.swift
+//  Test
+//
+//  Created by wangdongkai on 17/8/1.
+//  Copyright © 2017年 wangdongkai. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let path = UIBezierPath.init(arcCenter: self.center, radius: self.frame.midX / 2.0, startAngle: 0, endAngle: CGFloat(M_PI * 2), clockwise: true)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
